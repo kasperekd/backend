859547899371015381cdbc76c596df484d6d15d5
kasperekd <kasperekd@mail.ru>
2024-12-16 21:50:20 +0700

    new db cell_data TABLE

--- Database/init.sql
+++ Database/init.sql
@@ -1,6 +1,12 @@
-CREATE TABLE IF NOT EXISTS signal_data (
+CREATE TABLE cell_data (
     id SERIAL PRIMARY KEY,
-    device_id VARCHAR(50),
-    signal_strength INTEGER,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    type TEXT NOT NULL,
+    coordinates TEXT NOT NULL,
+    timestamp BIGINT NOT NULL,
+    cell_id BIGINT NOT NULL,
+    signal_strength INT NOT NULL,
+    tracking_area_code BIGINT NOT NULL,
+    operator TEXT NOT NULL,
+    rsrp INT NOT NULL,
+    rsrq INT NOT NULL
 );
